4a0945d1bd34e501d197701d58309e951aa8fcb2
Odoo <zeso@odoo.com>
2024-04-02 17:41:39 +0530

    [ADD] : Pre-commission
    - add commission rule menu in configuration
    - add sales commission menu in Reporting
    - add search, list, form view of both the menu

---
+++ sale_commission/models/sales_commissions.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class SalesCommission(models.Model):
+    _name = "sales.commissions"
+    
+    date = fields.Date()
+    sales_person_id = fields.Many2one('res.users')
+    sales_team_id = fields.Many2one('crm.team')
+    invoice_id = fields.Many2one('account.move')
+    currency_id = fields.Many2one('res.currency')
+    amount = fields.Monetary(string="Amount")
